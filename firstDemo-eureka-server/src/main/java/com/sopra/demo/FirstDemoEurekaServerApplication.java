4006aa90794d05ab59ae22b1bc22d496c80dc6f9
ALAOUI SEKKOURI Ghita <ghita.alaoui@soprabanking.com>
2019-01-03 11:36:48 +0100

    first push

---
+++ firstDemo-eureka-server/src/main/java/com/sopra/demo/FirstDemoEurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.sopra.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstDemoEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstDemoEurekaServerApplication.class, args);
+	}
+
+}
+
